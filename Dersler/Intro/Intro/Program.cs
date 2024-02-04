3886f143c047f63672e13efe00ed1c98818a3657
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2024-02-04 21:25:52 +0300

    Ders 1 Part 1 started.

---
+++ Dersler/Intro/Intro/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Hello, World!");
+// Consol.WriteLine() ile ekrana yazı yazdırıyoruz.
+// Tek satır yorum satırı
+/* Çoklu satır yorum satırı
+ *   1. satır
+ *   2. satır
+ *   3. satır
+ */
+
+string message1 = "Krediler";
+int term = 36;
+double amount = 100000.5;
+bool isAunthenticated = true;
+
+/**
+ * Metinsel ifadeler "" içerisinde tanımlanır.
+ * Değişken tanımlarken veriTürü değişkenAdı = değer; şeklinde tanımlanır.
+ * Değişken : Programın çalışma zamanında değişebilen değerlerdir.
+ * int : Tam sayı veri türüdür.
+ * double : Ondalıklı sayı veri türüdür. .5 veya tam sayı olabilir.
+ * bool : Mantıksal veri türüdür. true veya false değer alır.
+ Variable : Değişken 
+ * Değişken isimlendirirken camelCase kullanılır. İlk kelimenin baş harfi küçük, diğer kelimelerin baş harfi büyük olacak şekilde isimlendirilir.
+ */
+
+Console.WriteLine(message1);
