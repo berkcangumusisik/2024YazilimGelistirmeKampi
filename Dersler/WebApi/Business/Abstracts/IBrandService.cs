22da6a9670a667357f1925fb9c0c2af314518066
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2024-02-25 21:26:31 +0300

    Lesson 3 Started.

---
+++ Dersler/WebApi/Business/Abstracts/IBrandService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstracts;
+
+public interface IBrandService
+{
+    Brand Add(Brand brand);
+    List<Brand> GetAll();
+    
+}
+/**
+ Veri tabanı nesneleri ile iş katmanı arasında bir köprü görevi gören servis katmanıdır.Kullanıcıdan alınan verileri iş katmanına iletir ve iş katmanından gelen verileri kullanıcıya iletir. Kullanıcı ihtiyacı olan verileri servis katmanı aracılığı ile alır.
+ */
---
+++ Dersler/WebApi/DataAccess/Concretes/BrandDal.cs
@@ -0,0 +1,34 @@
+﻿using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concretes;
+
+public class BrandDal : IBrandDal
+{
+    List<Brand> _brands;
+    public BrandDal()
+    {
+        _brands = new List<Brand>
+        {
+            new Brand { Id = 1, Name = "Audi", CreatedDate=DateTime.Now },
+            new Brand { Id = 2, Name = "BMW", CreatedDate=DateTime.Now },
+            new Brand { Id = 3, Name = "Mercedes" , CreatedDate = DateTime.Now},
+            new Brand { Id = 4, Name = "Volkswagen", CreatedDate = DateTime.Now },
+            new Brand { Id = 5, Name = "Fiat" , CreatedDate = DateTime.Now}
+        };
+    }
+    public void Add(Brand brand)
+    {
+        _brands.Add(brand);
+    }
+
+    public List<Brand> GetAll()
+    {
+        return _brands;
+    }
+}
